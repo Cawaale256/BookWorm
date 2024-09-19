360635faa9962334414cec40bfa7109aea18f1c8
Abdi Awale <abdi.awale5@gmail.com>
2024-09-19 19:35:42 +0000

    Add models.py content

--- library/models.py
+++ library/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    join_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
