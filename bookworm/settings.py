20a4f94c23c5a9ee0182dc53c041f381599d3ef7
Abdi Awale <abdi.awale5@gmail.com>
2024-09-25 21:02:41 +0000

    Add Import load_dotenv from python-dotenv

--- bookworm/settings.py
+++ bookworm/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-
-# import environment variable from env.py property
-import env
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Define the templates directory
