6db0a8eac4d9b99e35f84a00df868bca9273701d
Abdi Awale <abdi.awale5@gmail.com>
2024-09-25 21:40:49 +0000

    Add code to admin.py

--- bookworm/settings.py
+++ bookworm/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 # Load environment variables from .env file
-load_dotenv()
+# load_dotenv()
+# Import environment variables from env.py
+import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Define the templates directory
