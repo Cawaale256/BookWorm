5ad0f7f0317e1844e54dd0791f49bde03ef3db45
Abdi Awale <abdi.awale5@gmail.com>
2024-09-26 10:54:49 +0000

    Define the MemberAdmin class

--- library/admin.py
+++ library/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import Book
+# from django.shortcuts import render, redirect, get_object_or_404
+# from django.urls import path
+# from django.utils.html import format_html
+from .models import Book, Member
+# from .forms import BorrowForm, ExtendForm
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'published_date', 'isbn')
     search_fields = ('title', 'author', 'isbn')
-
 admin.site.register(Book, BookAdmin)
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'join_date')
+    search_fields = ('first_name', 'last_name', 'email')
+admin.site.register(Member, MemberAdmin)    
+
