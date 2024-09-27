341279f2486a3e0a0be7cfd36540e851c1b6d1d3
Abdi Awale <abdi.awale5@gmail.com>
2024-09-27 11:17:03 +0000

    Change debug to true

--- bookworm/settings.py
+++ bookworm/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-cawaale256-bookworm-7wlk4d5e1u2.ws.codeinstitute-ide.net','.herokuapp.com']
 
