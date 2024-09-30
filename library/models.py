f4b30f46ceb3a26db666f95e7f8bafb7e9be23cc
Abdi Awale <abdi.awale5@gmail.com>
2024-09-30 19:56:06 +0000

    change all field in model book to null and blank to true

--- library/models.py
+++ library/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    published_date = models.DateField()
+    title = models.CharField(max_length=200, blank=True, null=True)
+    author = models.CharField(max_length=100, blank=True, null=True)
+    published_date = models.DateField(blank=True, null=True)
     isbn = models.CharField(max_length=13, unique=True)
     borrower = models.CharField(max_length=200, blank=True, null=True)
     borrow_date = models.DateField(blank=True, null=True)
