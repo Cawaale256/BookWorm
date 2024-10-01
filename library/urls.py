06623f2a9629058a04db918d23ea6f28f0aaaed3
Abdi Awale <abdi.awale5@gmail.com>
2024-10-01 10:19:05 +0000

    Add logout linkto base.html

--- library/urls.py
+++ library/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
 path('', views.home, name='home'),  # Home page
-    path('signup/', views.signup_view, name='signup'),
-    path('signin/', views.signin_view, name='signin'),
-    path('logout/', LogoutView.as_view(), name='logout'), 
+    path('signup/', views.signup_view, name='signup'),#signup
+    path('signin/', views.signin_view, name='signin'),# signin
+    path('logout/', LogoutView.as_view(), name='logout'), # logout
     path('books/', views.book_list, name='book_list'),  # Book list page
     path('books/<int:id>/', views.book_detail, name='book_detail'),  # Book detail page
     path('members/', views.member_list, name='member_list'),  # Member list page
