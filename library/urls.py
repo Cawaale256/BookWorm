42e79a149f9ccca39e1c66489e615b58544d7b13
Abdi Awale <abdi.awale5@gmail.com>
2024-10-03 20:35:55 +0000

    Add return url

--- library/urls.py
+++ library/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
     path('book/<int:pk>/borrow/', views.borrow_book, name='borrow_book'),
     path('book/<int:pk>/extend/', views.extend_due_date, name='extend_due_date'),
+    path('book/<int:pk>/return/', views.return_book, name='return_book'),
     path('dashboard/', views.user_dashboard, name='user_dashboard'),
     path('login/', views.signin_view, name='signin'),
     path('signup/', views.signup_view, name='signup'),
