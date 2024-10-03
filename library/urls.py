e8d9b1e251d8b457929de76f02a8ae89921b6189
Abdi Awale <abdi.awale5@gmail.com>
2024-10-03 11:04:48 +0000

    Add urls for borrowing,extending and the dashboard

--- library/urls.py
+++ library/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('book/new/', views.book_create, name='book_create'),
     path('book/<int:pk>/edit/', views.book_update, name='book_update'),
     path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
+    path('book/<int:pk>/borrow/', views.borrow_book, name='borrow_book'),
+    path('book/<int:pk>/extend/', views.extend_due_date, name='extend_due_date'),
+    path('dashboard/', views.user_dashboard, name='user_dashboard'),
 ]
+
