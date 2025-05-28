d923520a36c5cccf6b63a6e3c2e65437761cf38d
Abdi Awale <abdi.awale5@gmail.com>
2025-05-28 20:47:32 +0000

    Add SSL connection to databases

--- bookworm/settings.py
+++ bookworm/settings.py
@@ -95,8 +95,11 @@ WSGI_APPLICATION = 'bookworm.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600, ssl_require=True)
 }
 
 # Password validation
